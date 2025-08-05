0d079367166a9920291ca18d8cc4788f072ec3e0
kamathamkiran <kamathamkiran2004@gmail.com>
2025-08-05 11:51:40 +0530

    p2 added

--- src/main/java/com/p2/class2.java
+++ src/main/java/com/p2/class2.java
@@ -1,4 +1,13 @@
 package com.p2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class class2 {
+
+    @GetMapping("/hello2")
+    public String hello(){
+        return "Hello World from K.Kiran";
+    }
 }
