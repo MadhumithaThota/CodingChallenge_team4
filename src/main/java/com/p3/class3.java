00f57ec502c52fb9fe51ec085f2d43cc7c25d35c
Rakshanda23 <rakshandagiri123@gmail.com>
2025-08-05 11:56:42 +0530

    hello controller added in p3

--- src/main/java/com/p3/class3.java
+++ src/main/java/com/p3/class3.java
@@ -1,4 +1,12 @@
 package com.p3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class class3 {
+     @GetMapping("/helloR")
+    public String sayHello() {
+        return "Hello Rakshanda this side";
+    }
 }
