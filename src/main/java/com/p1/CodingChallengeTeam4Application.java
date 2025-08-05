34badcb20a3aeaf3860f9a8dfbedc3b5829e0062
Madhumitha <madhumithathota07@gmail.com>
2025-08-05 11:09:19 +0530

    Initial commit

---
+++ src/main/java/com/p1/CodingChallengeTeam4Application.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeTeam4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingChallengeTeam4Application.class, args);
+    }
+
+}
